b54a746a1aec8f0a3bf96e1c2038b18a4fd370bf
Justin Kaplan <justin.kaplan@gmail.com>
2015-11-10 15:35:52 -0500

    Set user permissions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ActiveRecord::Base
-	mount_uploader :image, ImageUploader
-	has_many :reviews
+  mount_uploader :image, ImageUploader
+
+  has_many :reviews
+
+  validates :name, :phone, :website, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+
 end
